8f612174bb0b82da558b0f63b0b066579708b2f1
john <john@johns-MacBook-Pro.local>
2019-06-19 23:35:40 -0600

    Added notes for the DumpListOfSQLTablesExample

--- src/main/scala/DumpListOfSQLTablesExample.scala
+++ src/main/scala/DumpListOfSQLTablesExample.scala
@@ -9,6 +9,32 @@
 //    wget https://raw.githubusercontent.com/ankane/movielens.sql/master/movielens.sql
 //    mysql -u root movielens < movielens.sql
 
+// This demos how to automate reading from a list of tables and
+// dumping the contents out to HDFS.
+//
+// In production you will need to tell spark how to partition the data,
+// so you can have more than one Spark executor working on a table.
+//
+// This will involve telling it which column to partition on (usually `id`)
+// and how many partitions you want.
+//
+// You also may want to repartition/coalesce to a smaller number of partitions
+// before writing to parquet, to avoid small files.
+//
+// Additional Info:
+//
+// https://medium.com/@radek.strnad/tips-for-using-jdbc-in-apache-spark-sql-396ea7b2e3d3
+//
+// https://dzone.com/articles/the-right-way-to-use-spark-and-jdbc
+//   - The code sample from this one can give you some good ideas about how to automate this:
+//
+//    val primaryKey = executeQuery(url, user, password, s"SHOW KEYS FROM ${config("schema")}.${config("table")} WHERE Key_name = 'PRIMARY'").getString(5)
+//    val result = executeQuery(url, user, password, s"select min(${primaryKey}), max(${primaryKey}) from ${config("schema")}.${config("table")}")
+//    val min = result.getString(1).toInt
+//    val max = result.getString(2).toInt
+//    val numPartitions = (max - min) / 5000 + 1
+
+
 import org.apache.spark.sql.{SaveMode, SparkSession}
 
 object DumpListOfSQLTablesExample {
@@ -17,6 +43,9 @@ object DumpListOfSQLTablesExample {
     val timezone = "GMT-6"
     val user = "root"
     val password = ""
+
+    // This is where you would handle calculating and telling Spark how to
+    // partition the data.
 
     spark.read
       .format("jdbc")
